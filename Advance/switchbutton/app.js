17804eba3e207482835a81c713dcd1a3398810bd
Knight <mayanks365@gmail.com>
2020-09-26 23:13:45 +0530

    Practice

---
+++ Advance/switchbutton/app.js
@@ -0,0 +1,35 @@
+// const change = document.querySelector(".switch");
+// const colors = ["#333", "#f4f4f4"];
+
+// change1.addEventListener("click", switchcolor);
+
+// function switchcolor(e) {
+//   document.body.style.backgroundColor = colors[e];
+//   console.log("Clicked");
+// }
+
+//  dark mode
+// const change1 = document.querySelector(".dark");
+// const colors1 = ["#333"];
+
+// change1.addEventListener("click", darkMode);
+
+// function darkMode() {
+//   document.body.style.backgroundColor = colors1;
+//   console.log("Clicked");
+// }
+
+// //  light mode
+// const change2 = document.querySelector(".light");
+// const colors2 = ["#f4f4f4"];
+
+// change2.addEventListener("click", lightMode);
+
+// function lightMode() {
+//   document.body.style.backgroundColor = colors2;
+//   console.log("Clicked");
+// }
+
+function myFunction() {
+  document.body.classList.toggle("dark-mode");
+}
