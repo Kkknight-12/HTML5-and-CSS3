d2fbb10a6f47ea0b792b3544a1f7c855af965bcc
Knight <mayanks365@gmail.com>
2021-03-10 23:35:01 +0530

    practice

--- positioning/d-positioning/app.js
+++ positioning/d-positioning/app.js
@@ -1,6 +1,7 @@
 var button = document.getElementById('open');
 var close = document.getElementById('close');
 var modal = document.getElementById('modal');
+// let container = document.body
 
 // when you open modal you put display type to block
 button.addEventListener('click', function (event) {   
@@ -13,3 +14,14 @@ close.addEventListener('click', function (event) {
     event.preventDefault();                               
     modal.style.display = 'none';                         
 }); 
+
+// container.addEventListener( 'click', function (event) {      
+//     event.preventDefault();    
+//     console.log('clicked')                           
+//     modal.style.display = 'none';
+// });
+
+const [firstTool, ...rest] = ['hammer', 'screwdriver', 'wrench']
+
+console.log(firstTool)
+console.log(rest)
